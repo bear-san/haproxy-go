46f6752f7debafc787283235b3e8fe36260c368c
Kentaro Abe <kentaro@beah.jp>
2025-02-12 01:07:20 +0900

    add: common client and errors

---
+++ dataplane/v3/error.go
@@ -0,0 +1,41 @@
+package v3
+
+type NotFoundError struct {
+	Message string
+}
+
+func (e *NotFoundError) Error() string {
+	return e.Message
+}
+
+type BadRequestError struct {
+	Message string
+}
+
+func (e *BadRequestError) Error() string {
+	return e.Message
+}
+
+type InvalidResponseError struct {
+	Message string
+}
+
+func (e *InvalidResponseError) Error() string {
+	return e.Message
+}
+
+type UnauthorizedError struct {
+	Message string
+}
+
+func (e *UnauthorizedError) Error() string {
+	return e.Message
+}
+
+type UnknownError struct {
+	Message string
+}
+
+func (e *UnknownError) Error() string {
+	return e.Message
+}
