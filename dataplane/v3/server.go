433175f9453a42a086d102789ace539fdc825143
Kentaro Abe <kentaro@beah.jp>
2025-03-04 01:50:54 +0900

    fix: missing path for replace server method

--- dataplane/v3/server.go
+++ dataplane/v3/server.go
@@ -69,9 +69,10 @@ func (c Client) ListServer(backend string, transactionId string) ([]Server, erro
 
 func (c Client) ReplaceServer(backend string, transactionId string, b Server) (*Server, error) {
 	apiUrl := fmt.Sprintf(
-		"%s/services/haproxy/configuration/backends/%s/servers?transaction_id=%s",
+		"%s/services/haproxy/configuration/backends/%s/servers/%s?transaction_id=%s",
 		c.BaseUrl,
 		backend,
+		*b.Name,
 		transactionId,
 	)
 
